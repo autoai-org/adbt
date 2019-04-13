07f39f79ac1b1521a46a2a217eab6a85a5a87d83
cyy <sheeppooo.cy@gmail.com>
2019-04-13 16:18:59 +0800

    add several

---
+++ app/src/components/Drawer.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-navigation-drawer
+    id="app-drawer"
+    v-model="drawer"
+    app
+    dark
+    floating
+    persistent
+    mobile-break-point="991"
+    width="260"
+  >
+    <v-img :src="image" height="100%">
+      <v-layout class="fill-height" tag="v-list" column>
+        <v-list-tile avatar>
+          <v-list-tile-avatar color="white">
+            <v-img :src="logo" height="34" contain/>
+          </v-list-tile-avatar>
+          <v-list-tile-title class="title">Vuetify MD</v-list-tile-title>
+        </v-list-tile>
+        <v-divider/>
+        <v-list-tile v-if="responsive">
+          <v-text-field class="purple-input search-input" label="Search..." color="purple"/>
+        </v-list-tile>
+        <v-list-tile
+          v-for="(link, i) in links"
+          :key="i"
+          :to="link.to"
+          :active-class="color"
+          avatar
+          class="v-list-item"
+        >
+          <v-list-tile-action>
+            <v-icon>{{ link.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title v-text="link.text"/>
+        </v-list-tile>
+        <v-list-tile
+          disabled
+          active-class="primary"
+          class="v-list-item v-list__tile--buy"
+          to="/upgrade"
+        >
+          <v-list-tile-action>
+            <v-icon>mdi-package-up</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title class="font-weight-light">Upgrade To PRO</v-list-tile-title>
+        </v-list-tile>
+      </v-layout>
+    </v-img>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+    props:["open"]
+};
+</script>
+
+<style>
+</style>
